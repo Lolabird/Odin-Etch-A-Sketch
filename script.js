06aa9203afbbde3d2410d0ed067e4617b512eb48
lolabird <octavia@octavianichols.com>
2023-11-09 16:31:23 -0700

    Move grid creation to function

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const container = document.querySelector(".container");
-const conWidth = container.clientWidth;
-const conHeight = container.clientHeight
-const grid = document.createElement("div");
-let numGrid = 16;
-const gridArea = numGrid ** 2;
-const gridWidth = conWidth / numGrid;
-const gridHeight = conHeight / numGrid;
+function createGrid(num) {
+    const container = document.querySelector(".container");
+    const conWidth = container.clientWidth;
+    const conHeight = container.clientHeight
+    const grid = document.createElement("div");
+    const gridArea = num ** 2;
+    const gridWidth = conWidth / num;
+    const gridHeight = conHeight / num;
 
-grid.classList.add("grid");
-grid.style.width = gridWidth + 'px';
-grid.style.height = gridHeight + 'px';
+    grid.classList.add("grid");
+    grid.style.width = gridWidth + 'px';
+    grid.style.height = gridHeight + 'px';
 
-for (let i = 0; i < gridArea; i++){
-    container.appendChild(grid.cloneNode(true));
+    for (let i = 0; i < gridArea; i++){
+        container.appendChild(grid.cloneNode(true));
+    }
 }
+
+let numGrid = 16; //will use this later to prompt user
+
+createGrid(numGrid);
+
+
