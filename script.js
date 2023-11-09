85ef7a2709843d5b9297783d95d36af541605b38
lolabird <octavia@octavianichols.com>
2023-11-09 15:14:02 -0700

    Add grid

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ const conWidth = container.clientWidth;
 const conHeight = container.clientHeight
 const grid = document.createElement("div");
 let numGrid = 16;
+const gridArea = numGrid ** 2;
 const gridWidth = conWidth / numGrid;
 const gridHeight = conHeight / numGrid;
+
+grid.classList.add("grid");
+grid.style.width = gridWidth + 'px';
+grid.style.height = gridHeight + 'px';
+
+for (let i = 0; i < gridArea-1; i++){
+    container.appendChild(grid.cloneNode(true));
+}
